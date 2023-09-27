1b6c75ead1a04463b16cd623b642f9fa688eb3a6
Bismitha <bismitha@gmail.com>
2023-09-27 00:22:46 -0700

    first commit

---
+++ src/test/java/Addition_Test.java
@@ -0,0 +1,15 @@
+
+public class Addition_Test {
+	
+	
+	float a=250;
+	float b=487;
+	float c=30;
+
+	float add = a+b+c;
+	
+	public static void main(String[]args) {
+		
+System.out.println("Addition Test");
+	}
+}
